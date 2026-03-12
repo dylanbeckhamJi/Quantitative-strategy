1da4f8bd6b1384240c9683645b62d4a586d99f69
Jzcheng <1378614475@qq.com>
2026-03-12 19:48:57 +0800

    Update 常用.py

--- 常用.py
+++ 常用.py
@@ -1,5 +1,15 @@
-from __future__ import print_function, absolute_import #
+from __future__ import print_function, absolute_import 
 from gm.api import *
 from gm.model.storage import context
 import numpy as np
 import pandas as pd
+from datetime import  datetime,timezone, timedelta
+import datetime
+pd.set_option('display.max_rows', None) #show all rows
+pd.set_option('display.float_format', '{:.2f}'.format)
+import warnings
+warnings.filterwarnings('ignore')
+
+#掘金量化
+set_token('807b8ba88782d7343bfe3ad918f33f93a2610ee8')
+account_id='28edb8c3-ed6d-11f0-9ac0-00163e022aa6'
